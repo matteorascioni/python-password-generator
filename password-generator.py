a4e776d8e7faa6749440eaa135327272c3d00aed
Matteo <matteorascioni@gmail.com>
2023-01-30 21:40:28 +0100

    fixing comment

--- password-generator.py
+++ password-generator.py
@@ -4,7 +4,7 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-#new list necessary to append the random choise (see line 19)
+#new list necessary to append the random choise (see line 25)
 new_arr = []
 #initialing the password as an empty string
 password = ''
